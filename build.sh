58bbfb98396ff26d5d0d32421bc5752bf2d937d5
creio <mailcreio@gmail.com>
2021-10-30 23:15:02 +0300

    30.10.2021 Update

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 image_name="archlinux-$(date +%Y%m%d)-x86_64.iso"
 
 echo "install pkg"
-pacman -S git archiso mkinitcpio-archiso --noconfirm --needed
+pacman -Syy git archiso mkinitcpio-archiso --noconfirm --needed
 
 echo "build iso"
 mkarchiso -v -w ./work -o ./out /usr/share/archiso/configs/releng/
@@ -11,4 +11,5 @@ mkarchiso -v -w ./work -o ./out /usr/share/archiso/configs/releng/
 if [[ -e "./out/$img_name" ]]; then
   echo "create SHA 256"
   sha256sum out/$image_name >> out/$image_name.sha256
+  export image_name=$image_name
 fi
