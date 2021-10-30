d17c11030451d0695be28a21c3bd8c98864237bc
creio <mailcreio@gmail.com>
2021-10-31 00:34:28 +0300

    31.10.2021 Update

--- build.sh
+++ build.sh
@@ -6,10 +6,10 @@ echo "install pkg"
 pacman -Syy git archiso mkinitcpio-archiso --noconfirm --needed
 
 echo "build iso"
-mkarchiso -v -w ./work -o ./out /usr/share/archiso/configs/releng/
+mkarchiso -v -w ./work -o /out /usr/share/archiso/configs/releng/
 
-if [[ -e "./out/$img_name" ]]; then
+if [[ -e "/out/$img_name" ]]; then
   echo "create SHA 256"
-  sha256sum ./out/$image_name >> ./out/$image_name.sha256
+  sha256sum /out/$image_name >> /out/$image_name.sha256
   echo "image_name=$image_name" >> $GITHUB_ENV
 fi
